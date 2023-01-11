e3aa700cf843083e01f49249d7739e8e75d01769
Kleb <94545082+Spartan2909@users.noreply.github.com>
2023-01-11 10:19:07 +0000

    Scaffold project

---
+++ cl-core/src/lib.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn read_file(path: &str) -> io::Result<String> {
+    // Create a path to the desired file
+    let path = Path::new(path);
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = File::open(&path)?;
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
