8f35c4e6a2f3a3fb025bb462307aa6f92d26763c
Kleb <94545082+Spartan2909@users.noreply.github.com>
2023-01-11 13:53:35 +0000

    Move scanner to separate module

--- cl-core/src/lib.rs
+++ cl-core/src/lib.rs
@@ -2,34 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum TokenType {
-    // Single character tokens
-    FullStop, QuestionMark, LeftParen, RightParen,
-
-    // Reserved words
-    Article, Operator, Prepostion, Verb,
-    
-    // Identifiers
-    Literal, Variable,
-
-    EOF
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Token {
-    kind: TokenType,
-    lexeme: String,
-    start: usize,
-    length: usize,
-}
-
-impl Token {
-    fn new(kind: TokenType, lexeme: String, start: usize, length: usize) -> Self {
-        Token { kind, lexeme, start, length }
-    }
-}
+mod scanner;
 
 pub fn read_file(path: &str) -> io::Result<String> {
     let path = Path::new(path);
@@ -40,86 +13,7 @@ pub fn read_file(path: &str) -> io::Result<String> {
     Ok(s)
 }
 
-fn valid_iden(c: char) -> bool {
-    c.is_alphabetic() || c == '-' || c == '_'
-}
-
-fn get_word(source: &str, start: usize) -> String {
-    let mut end = start;
-    while end < source.len() && valid_iden(source.chars().nth(end).unwrap()) {
-        end += 1;
-    }
-
-    String::from(&source[start..end])
-}
-
-fn scan(source: String) -> Vec<Token> {
-    let mut tokens = Vec::new();
-
-    let mut i: usize = 0;
-    while i < source.len() {
-        let c = source.chars().nth(i).unwrap();
-        match c {
-            c if c.is_whitespace() => {i += 1; continue},
-            '(' => tokens.push(Token::new(TokenType::LeftParen, String::from("("), i, 1)),
-            ')' => tokens.push(Token::new(TokenType::RightParen, String::from(")"), i, 1)),
-            '.' => tokens.push(Token::new(TokenType::FullStop, String::from("."), i, 1)),
-            '?' => tokens.push(Token::new(TokenType::QuestionMark, String::from("?"), i, 1)),
-            c if c.is_alphabetic() => {
-                let lexeme = get_word(&source, i);
-                let length = lexeme.len();
-                let kind: TokenType;
-                match lexeme.to_lowercase().as_str() {
-                    "a" | "an" | "the" => kind = TokenType::Article,
-                    "and" | "or" => kind = TokenType::Operator,
-                    "of" | "to" => kind = TokenType::Prepostion,
-                    "is" | "are" => kind = TokenType::Verb,
-                    _ => {
-                        if lexeme.chars().nth(length-1).unwrap().is_lowercase() {
-                            kind = TokenType::Literal;
-                        } else {
-                            kind = TokenType::Variable;
-                        }
-                    }
-                }
-                tokens.push(Token::new(kind, lexeme, i, length));
-
-                i += length;
-                continue
-            },
-            _ => todo!("unrecognised character"),
-        }
-
-        i += 1;
-    }
-
-    tokens.push(Token::new(TokenType::EOF, String::from(""), i, 0));
-    tokens
-}
-
 pub fn cl_to_prolog(source: String) -> String {
-    let tokens = scan(source);
+    let tokens = scanner::scan(source);
     String::from("")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::collections::HashSet;
-    use crate::TokenType::*;
-
-    #[test]
-    fn scan_test() {
-        assert_eq!(HashSet::from_iter(scan(String::from("A hamster is a mammal."))),
-            HashSet::from([
-                Token { kind: Article, lexeme: String::from("A"), start: 0, length: 1 },
-                Token { kind: Literal, lexeme: String::from("hamster"), start: 2, length: 7 },
-                Token { kind: Verb, lexeme: String::from("is"), start: 10, length: 2 },
-                Token { kind: Article, lexeme: String::from("a"), start: 13, length: 1 },
-                Token { kind: Literal, lexeme: String::from("mammal"), start: 15, length: 6 },
-                Token { kind: FullStop, lexeme: String::from("."), start: 21, length: 1 },
-                Token { kind: EOF, lexeme: String::from(""), start: 22, length: 0 }
-            ])
-        )
-    }
 }
---
+++ cl-core/src/scanner.rs
@@ -0,0 +1,107 @@
+use self::TokenType::*;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum TokenType {
+    // Single character tokens
+    FullStop, QuestionMark, LeftParen, RightParen,
+
+    // Reserved words
+    Article, Operator, Prepostion, Verb,
+    
+    // Identifiers
+    Literal, Variable,
+
+    EOF
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Token {
+    kind: TokenType,
+    lexeme: String,
+    start: usize,
+    length: usize,
+}
+
+impl Token {
+    fn new(kind: TokenType, lexeme: String, start: usize, length: usize) -> Self {
+        Token { kind, lexeme, start, length }
+    }
+}
+
+fn valid_iden(c: char) -> bool {
+    c.is_alphabetic() || c == '-' || c == '_'
+}
+
+fn get_word(source: &str, start: usize) -> String {
+    let mut end = start;
+    while end < source.len() && valid_iden(source.chars().nth(end).unwrap()) {
+        end += 1;
+    }
+
+    String::from(&source[start..end])
+}
+
+pub fn scan(source: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    let mut i: usize = 0;
+    while i < source.len() {
+        let c = source.chars().nth(i).unwrap();
+        match c {
+            c if c.is_whitespace() => {i += 1; continue},
+            '(' => tokens.push(Token::new(LeftParen, String::from("("), i, 1)),
+            ')' => tokens.push(Token::new(RightParen, String::from(")"), i, 1)),
+            '.' => tokens.push(Token::new(FullStop, String::from("."), i, 1)),
+            '?' => tokens.push(Token::new(QuestionMark, String::from("?"), i, 1)),
+            c if c.is_alphabetic() => {
+                let lexeme = get_word(&source, i);
+                let length = lexeme.len();
+                let kind: TokenType;
+                match lexeme.to_lowercase().as_str() {
+                    "a" | "an" | "the" => kind = Article,
+                    "and" | "or" => kind = Operator,
+                    "of" | "to" => kind = Prepostion,
+                    "is" | "are" => kind = Verb,
+                    _ => {
+                        if lexeme.chars().nth(length-1).unwrap().is_lowercase() {
+                            kind = Literal;
+                        } else {
+                            kind = Variable;
+                        }
+                    }
+                }
+                tokens.push(Token::new(kind, lexeme, i, length));
+
+                i += length;
+                continue
+            },
+            _ => todo!("unrecognised character"),
+        }
+
+        i += 1;
+    }
+
+    tokens.push(Token::new(TokenType::EOF, String::from(""), i, 0));
+    tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn scan_test() {
+        assert_eq!(HashSet::from_iter(scan(String::from("A hamster is a mammal."))),
+            HashSet::from([
+                Token { kind: Article, lexeme: String::from("A"), start: 0, length: 1 },
+                Token { kind: Literal, lexeme: String::from("hamster"), start: 2, length: 7 },
+                Token { kind: Verb, lexeme: String::from("is"), start: 10, length: 2 },
+                Token { kind: Article, lexeme: String::from("a"), start: 13, length: 1 },
+                Token { kind: Literal, lexeme: String::from("mammal"), start: 15, length: 6 },
+                Token { kind: FullStop, lexeme: String::from("."), start: 21, length: 1 },
+                Token { kind: EOF, lexeme: String::from(""), start: 22, length: 0 }
+            ])
+        )
+    }
+}
