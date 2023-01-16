6c2f28702185a56d0ecc68ee31c020463d7a176b
Kleb <94545082+Spartan2909@users.noreply.github.com>
2023-01-16 17:29:21 +0000

    Add test program

--- colog/src/main.rs
+++ colog/src/main.rs
@@ -1,5 +1,9 @@
 use cl_core::transpile;
 
 fn main() {
-    transpile(String::from(""));
+    transpile(String::from("
+John is the sibling of Jack.
+John is male.
+X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Z."
+    ));
 }
