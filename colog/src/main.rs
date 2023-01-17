324d5332bc1e3723e95456c05c77fac0f61f2368
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-17 21:20:39 +0000

    Fix typo

--- colog/src/main.rs
+++ colog/src/main.rs
@@ -8,6 +8,6 @@ John is male.
     transpile(String::from("
 John is the sibling of Jack.
 John is male.
-X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Z."
+X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Y."
     ));
 }
