9f968d84d231e9151e6bd909ec62288e8b3052a1
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-17 09:41:08 +0000

    Remove unneeded

--- cl-core/src/parser/ast.rs
+++ cl-core/src/parser/ast.rs
@@ -57,31 +57,3 @@ pub struct Stmt {
     pub right: Option<Identifier>,
     pub condition: Option<Clause>
 }
-
-impl Stmt {
-    pub fn fact_unary(left: scanner::Token, relationship: scanner::Token) -> Result<Self, super::ParseError> {
-        let left = Identifier::try_from(left)?;
-        let relationship = Identifier::try_from(relationship)?;
-        Ok(Self { kind: StmtType::Fact, left, relationship, right: None, condition: None })
-    }
-
-    pub fn fact_binary(left: scanner::Token, relationship: scanner::Token, right: scanner::Token) -> Result<Self, super::ParseError> {
-        let left = Identifier::try_from(left)?;
-        let relationship = Identifier::try_from(relationship)?;
-        let right = Identifier::try_from(right)?;
-        Ok(Self { kind: StmtType::Fact, left, relationship, right: Some(right), condition: None })
-    }
-
-    pub fn rule_unary(left: scanner::Token, relationship: scanner::Token) -> Result<Self, super::ParseError> {
-        let left = Identifier::try_from(left)?;
-        let relationship = Identifier::try_from(relationship)?;
-        Ok(Self { kind: StmtType::Rule, left, relationship, right: None, condition: None })
-    }
-
-    pub fn rule_binary(left: scanner::Token, relationship: scanner::Token, right: scanner::Token) -> Result<Self, super::ParseError> {
-        let left = Identifier::try_from(left)?;
-        let relationship = Identifier::try_from(relationship)?;
-        let right = Identifier::try_from(right)?;
-        Ok(Self { kind: StmtType::Rule, left, relationship, right: Some(right), condition: None })
-    }
-}
