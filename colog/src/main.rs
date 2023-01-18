806071a5aec454ccaa80975fed28bb67481fd2a0
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-18 09:15:42 +0000

    Fix logic in test

--- colog/src/main.rs
+++ colog/src/main.rs
@@ -6,8 +6,8 @@ John is the sibling of Jack.
 John is male.
     "));*/
     transpile(String::from("
-John is the sibling of Jack.
-John is male.
+John is the parent of Jack.
+John is the parent of Jane.
 X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Y."
     ));
 }
