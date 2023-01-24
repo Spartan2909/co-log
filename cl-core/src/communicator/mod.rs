56c621b95d8c87c31eb7425676d119ae716d4a95
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-24 20:07:11 +0000

    Cleaned up Prolog starting

--- cl-core/src/communicator/mod.rs
+++ cl-core/src/communicator/mod.rs
@@ -24,16 +24,19 @@ pub fn start_prolog() {
     let mut path = current_exe().unwrap();
     path.pop();
     path.push("temp.pl");
-    let trimmed = remove_prefix(path.to_str().unwrap());
-    unsafe {
-        let argv = ["swipl".as_ptr() as *mut i8, trimmed.as_ptr() as *mut i8].as_mut_ptr();
-        PL_initialise(2, argv);
-        //PL_predicate("name".as_ptr() as *const i8, 4, "module".as_ptr() as *const i8);
-    }
 
-    println!("test");
+    let trimmed = remove_prefix(path.to_str().unwrap());
+    let argv = ["swipl".as_ptr() as *mut i8, trimmed.as_ptr() as *mut i8].as_mut_ptr();
+
+    unsafe { PL_initialise(2, argv); }
+
+    println!("started prolog");
 }
 
 pub fn query_prolog() {
 
+}
+
+pub fn kill_prolog() {
+    unsafe { PL_halt(0); }
 }
