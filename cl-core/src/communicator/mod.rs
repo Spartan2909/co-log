ce1085ea01e6dbea2608bf09496c5c8c3b3900d5
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-24 15:00:27 +0000

    Pass path to Prolog

--- cl-core/src/communicator/mod.rs
+++ cl-core/src/communicator/mod.rs
@@ -1,4 +1,5 @@
-use std::ptr::{null, null_mut};
+use std::env::current_exe;
+use std::path::PathBuf;
 use swipl_fli::*;
 
 struct Cleanup;
@@ -12,26 +13,17 @@ impl Drop for Cleanup {
 
 const _CLEANUP: Cleanup = Cleanup;
 
-fn str_to_ptr(s: &str) -> *const i8 {
-    return s.as_ptr() as *const i8
-}
-
-fn str_to_mut_ptr(s: &str) -> *mut i8 {
-    return s.as_ptr() as *mut i8
-}
-
-unsafe fn consult_file(filename: &str) {
-    let p = PL_predicate(str_to_ptr("consult"), 1, null());
-    let a = PL_new_term_ref();
-    PL_put_atom_chars(a, str_to_ptr(filename));
-    PL_call_predicate(null_mut(), PL_Q_NORMAL.try_into().unwrap(), p, a);
+fn path_to_mut_ptr(p: PathBuf) -> *mut i8 {
+    p.to_str().unwrap().as_ptr() as *mut i8
 }
 
 pub fn start_prolog() {
+    let mut path = current_exe().unwrap();
+    path.pop();
+    path.push("temp.pl");
     unsafe {
-        let argv = [str_to_mut_ptr("consult_file"), str_to_mut_ptr("-q")].as_mut_ptr();
-        PL_initialise(2, null_mut());
-        //consult_file("filename");
+        let argv = ["swipl".as_ptr() as *mut i8, path_to_mut_ptr(path)].as_mut_ptr();
+        PL_initialise(2, argv);
         //PL_predicate("name".as_ptr() as *const i8, 4, "module".as_ptr() as *const i8);
     }
 
