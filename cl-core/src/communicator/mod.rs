cf11a1ddb8db17d35ed6c30b5b1052e8ba0bc185
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-25 20:53:28 +0000

    Begin querying prolog

--- cl-core/src/communicator/mod.rs
+++ cl-core/src/communicator/mod.rs
@@ -33,8 +33,30 @@ pub fn start_prolog() {
     println!("started prolog");
 }
 
-pub fn query_prolog() {
+pub fn query_prolog(query: &str) {
+    unsafe {
+        let t = PL_new_term_refs(1);
+        PL_put_atom_chars(t, "a".as_ptr() as *const i8);
+        let pred = PL_predicate("one".as_ptr() as *mut i8, 1, std::ptr::null());
+        //PL_put_atom_chars(t, "".as_ptr() as *mut i8);
+        let query = PL_open_query(std::ptr::null_mut(), PL_Q_CATCH_EXCEPTION as i32, pred, t);
 
+        let mut solns = Vec::new();
+        loop {
+            let soln = PL_next_solution(query);
+            if soln == FALSE as i32 {
+                break;
+            } else {
+                solns.push(soln);
+            }
+        }
+
+        dbg!(*query);
+
+        PL_cut_query(query);
+
+        dbg!(solns);
+    }
 }
 
 pub fn kill_prolog() {
