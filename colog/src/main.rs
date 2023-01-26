72c73d9fc80200f219405ee7a87f8049784bb710
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-01-26 15:20:40 +0000

    Fix tests

--- colog/src/main.rs
+++ colog/src/main.rs
@@ -1,25 +1,5 @@
 use cl_core;
-
 fn main() {
-    /*transpile(String::from("
-John is the sibling of Jack.
-John is male.
-    "));*/
-    /*transpile(String::from("
-John is the parent of Jack.
-John is the parent of Jane.
-X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Y."
-    ));*/
-    /*let r = transpile(String::from("
-        John is the sibling of Jack.
-        John is male.
-        X is the brother of Y if X is the sibling of Y and X is male.
-        Is John the brother of Jack?
-    "));*/
     let pl = cl_core::transpile(String::from("X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Y.")).unwrap();
-    cl_core::communicator::start_prolog();
-    //let r = cl_core::communicator::start_prolog("C:/Users/caleb/Documents/GitHub/co-log/test.pl", "call_nth((thing(X, Y), write(X), write(\",\"), writeln(Y)), 100).");
-    //dbg!(r);
-    //println!("{}", r.unwrap().0);
-    println!("test");
+    cl_core::communicator::start_prolog("worskspaces/co-log/target/debug/temp.pl");
 }
