41b4a5f4f2136889c1d2918e8b14ff5b0dacb482
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-02-22 20:34:48 +0000

    Remove old main function

--- colog/src/main.rs
+++ colog/src/main.rs
@@ -245,18 +245,3 @@ async fn main() -> Result<(), sqlx::Error> {
         }
     }
 }
-
-/*
-fn _old_main() {
-    env::set_var("RUST_BACKTRACE", "full");
-    let _pl = cl_core::transpile(String::from("X is the sibling of Y if Z is the parent of X and Z is the parent of Y and X is not Y.")).unwrap();
-    let mut path = env::current_exe().unwrap();
-    path.pop();
-    path.push("temp.pl");
-    let context = cl_core::communicator::start_prolog(path.to_str().unwrap()).unwrap();
-    cl_core::communicator::query_prolog(context, cl_core::transpiler::Query {
-        left: "l2".to_string(),
-        relationship: "l1".to_string(),
-        right: None
-    }).unwrap();
-}*/
